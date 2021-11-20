cde1a32b0f7e551b8e51c5154a1896c8b8a7dd04
宾梓翔 <binzixiang@163.com>
2021-11-20 19:42:35 +0800

    给定数组的最大值

---
+++ text_11.20(求给定数组的最大值）/text_11.209/求最大值.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	while (true)
+	{
+	printf("请给出所要求的数组\n");
+
+	int a = 0;
+
+	scanf("%d", &a);
+
+	int arr[100000] = { 0 };
+
+	printf("请您输入数组\n");
+
+	int i = 0;
+
+	for (i = 0; i < a ; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("您输入的数组为:\n");
+
+	for (i = 0; i < a ; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+	int max = 0;
+
+	for (i = 0; i < a; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	printf("最大值为:%d\n", max);
+
+	}
+	return 0;
+}
